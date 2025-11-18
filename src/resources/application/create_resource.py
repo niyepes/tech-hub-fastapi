317344bd10fd5bf00043d4c444220e100e07117c
Nicolas <nayepesc@udistrital.edu.co>
2025-11-18 10:06:29 -0500

    fix: Se agregó el import de Resource, que impedía que el test para create_resource, se pasara correctamente

--- src/resources/application/create_resource.py
+++ src/resources/application/create_resource.py
@@ -1,8 +1,9 @@
 from resources.domain.repositories import ResourceRepository
+from resources.domain.models import Resource
 
 class CreateResource:
     def __init__(self, resource_repository: ResourceRepository):
         self._resource_repository = resource_repository
+
     def execute(self) -> None:
-        # Logic to create a new resource
-        pass
+        self._resource_repository.save(Resource())
