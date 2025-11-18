42a9ab60d64289b9d035d9ea0448e6d5375018ce
Nicolas <nayepesc@udistrital.edu.co>
2025-11-18 10:20:13 -0500

    Red test for resource url

--- src/resources/application/create_resource.py
+++ src/resources/application/create_resource.py
@@ -1,9 +1,14 @@
 from resources.domain.repositories import ResourceRepository
 from resources.domain.models import Resource
+from dataclasses import dataclass
+
+@dataclass
+class CreateResourceCommand:
+    resource_url : str
 
 class CreateResource:
     def __init__(self, resource_repository: ResourceRepository):
         self._resource_repository = resource_repository
 
-    def execute(self) -> None:
+    def execute(self, command: CreateResourceCommand) -> None:
         self._resource_repository.save(Resource())
--- src/shared/tests/application/test_create_resource.py
+++ src/shared/tests/application/test_create_resource.py
@@ -1,5 +1,6 @@
 from resources.application.create_resource import CreateResource
 from resources.domain.models import Resource
+from resources.application.create_resource import CreateResourceCommand
 
 class FakeResourceRepository:
     def __init__(self) -> None:
@@ -14,7 +15,10 @@ class FakeResourceRepository:
 class TestCreateResource:
     def test_create_resource(self) -> None:
         resource_repository = FakeResourceRepository()
-        CreateResource(resource_repository).execute()
-
-        assert len(resource_repository.all()) == 1
+        CreateResource(resource_repository).execute(
+            CreateResourceCommand(resource_url="resource-url-example")
+        )
+        resources = resource_repository.all()
+        assert len(resources) == 1
+        assert resources[0].url == "resource-url-example"
 
