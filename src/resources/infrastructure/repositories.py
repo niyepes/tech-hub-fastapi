b8e631466db17eb97eb10535b0bf369c6697d636
Nicolas <nayepesc@udistrital.edu.co>
2025-11-20 09:53:33 -0500

    fix: Se corrigieron los errores que impedían que se pasara el test de guardar recursos en la base de datos

--- src/resources/infrastructure/repositories.py
+++ src/resources/infrastructure/repositories.py
@@ -1,9 +1,20 @@
+from sqlmodel import SQLModel, Field, create_engine, Session
 from resources.domain.models import Resource
 from resources.domain.repositories import ResourceRepository
+
+class ResourceModel(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    url: str
+
+engine = create_engine("sqlite:///database.db")
+SQLModel.metadata.create_all(engine)
 
 class SQLModelResourceRepository(ResourceRepository):
     def all():
         pass
 
-    def save():
-        pass
+    def save(self, resource: Resource) -> None:
+        resource_model = ResourceModel(url=resource.get_url().value)
+        with Session(engine) as session:
+            session.add(resource_model)
+            session.commit()
--- src/shared/tests/infrastructure/repositories/test_sql_model_resource_repository.py
+++ src/shared/tests/infrastructure/repositories/test_sql_model_resource_repository.py
@@ -1,5 +1,7 @@
+from sqlmodel import Session, select
 from resources.domain.models import Resource
 from resources.domain.value_objects import ResourceUrl
+from resources.infrastructure.repositories import SQLModelResourceRepository, engine, ResourceModel
 
 class TestSQLModelResourceRepository:
     
@@ -8,10 +10,10 @@ class TestSQLModelResourceRepository:
         #Arrange/Given
         repo = SQLModelResourceRepository()
         #Act/When
-        repo.save(Resource(ResourceUrl("https://google.com")))
+        repo.save(Resource(ResourceUrl(value = "https://google.com")))
 
         #Assert/Then
         with Session(engine) as session:
             statement = select(ResourceModel)
-            resource = session.exec(statement)
-            esource.url == "https://google.com"
+            resource = session.exec(statement).first()
+            resource.url == "https://google.com"
